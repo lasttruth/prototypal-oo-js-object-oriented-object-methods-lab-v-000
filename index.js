f66fe18c62c9a006fa3849fcd7f9ceefb5a83874
Robin Doirin <robindoirin@gmail.com>
2019-11-01 21:57:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
+
+BoardMember.prototype.veto = function () {
+  return "No, I must disagree"
+};
+
+BoardMember.prototype.approve = function () {
+  return "You cna do that!"
+};
+
+BoardMember.prototype.doCharity = function () {
+  return "I like to help poeple."
+};
+
+BoardMember.prototype.releasePressStatement = function () {
+  return "You will see greeat things from Scuber."
+};
+
+BoardMember.prototype.sayHi = function () {
+  return "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}"
+};
