5467927052a9e31bc23152b923b5d9475eff2030
Robin Doirin <robindoirin@gmail.com>
2019-11-01 21:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ BoardMember.prototype.veto = function () {
 };
 
 BoardMember.prototype.approve = function () {
-  return "You cna do that!"
+  return "You can do that!"
 };
 
 BoardMember.prototype.doCharity = function () {
